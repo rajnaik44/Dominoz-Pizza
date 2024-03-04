56b520d14b887a49e30a745b5d5c9227b9d2a038
Raj Naik <raj44.naik@gmail.com>
2024-03-04 15:30:25 +0530

    made ui responsive

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
         <div className=" w-full h-auto mx-auto py-24 pl-[10%] p-2 flex  bg-green-50 ">
-        <div className='ml-[18%] grid grid-cols-3 gap-4'>
+        <div className='ml-[18%] mt-4 flex-col md:grid md:grid-cols-2 lg:grid-cols-3  gap-4'>
 
         {/* <Cards name="Hand Tossed" size="Mediun" price="$20.99" />
         <Cards name="Hand Tossed" size="small" price="$7.99" />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="fixed w-full h-20 shadow-xl z-[100]" >
     <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <h1 className='text-4xl cursor-pointer'>Product</h1>
-        <ul className='flex gap-8'>
+        <ul className='hidden md:flex gap-8'>
             <li className='font-bold text-xl hover:scale-75 ease-in duration-150'>Home</li>
             <li className='font-bold text-xl hover:scale-75 ease-in duration-150'>About</li>
             <li className='font-bold text-xl hover:scale-75 ease-in duration-150'>Contact</li>
