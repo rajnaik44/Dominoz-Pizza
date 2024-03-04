089a2a8fbc42c661c1379697a493160dcc493b97
Raj Naik <raj44.naik@gmail.com>
2024-03-04 17:16:25 +0530

    added data

--- app/data.jsx
+++ app/data.jsx
@@ -3,9 +3,13 @@ export const data = [
   { name: "Hand Tossed", size: "small", price: "$7.99" },
   { name: "Crunchy Thin", size: "large", price: "$9.99" },
   { name: "Cheeses Pizza", size: "x-large", price: "$7.99" },
+  { name: "Cheeses Pizza", size: "x-large", price: "$7.99" },
+  { name: "Crust Cheeses", size: "x-small", price: "$5.99" },
   { name: "Crust Cheeses", size: "x-small", price: "$5.99" },
   { name: "Hand Tossed", size: "small", price: "$7.99" },
   { name: "Crunchy Thin", size: "large", price: "$11.99" },
   { name: "Hand Tossed", size: "small", price: "$7.99" },
+  { name: "Crunchy Thin", size: "large", price: "$12.99" },
+  { name: "Crunchy Thin", size: "large", price: "$12.99" },
   { name: "Crunchy Thin", size: "large", price: "$12.99" },
 ];
